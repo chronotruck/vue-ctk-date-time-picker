6a56025eff6953f45a387e2d356354d6b8176b7e
gaabora <gaabora@gmail.com>
2018-11-29 12:55:56 +0300

    locale changes does not work
    In my case (moment 2.22.2) moment.tz(this.timeZone) somehow does not return a moment instance, so .locale(this.locale) wont work.

--- src/vue-ctk-date-time-picker/vue-ctk-date-time-picker.vue
+++ src/vue-ctk-date-time-picker/vue-ctk-date-time-picker.vue
@@ -199,7 +199,8 @@
       } else if (this.rangeMode) {
         this.$emit('input', this.getRangeDatesTimeFormat({}))
       }
-      moment.tz(this.timeZone).locale(this.locale)
+      moment.tz(this.timeZone)
+      moment.locale(this.locale)
     },
     methods: {
       getDateTime () {
