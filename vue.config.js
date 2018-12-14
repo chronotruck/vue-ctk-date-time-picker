ed2631bfac628827646f79863c0bcb212a5c5255
Mazel <mazuel.loic@gmail.com>
2018-12-14 18:34:29 +0100

    Hotfix css file name

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,9 @@ module.exports = {
     watchOptions: {
       poll: true
     }
-  }
+  },
+  config.plugin('extract-css').tap(([options]) => {
+    options.filename = path.join('your/relative/asset/path', options.filename)
+    return [options]
+  })
 }
