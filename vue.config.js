6504c5b723d4403a900f984c266cd16823d4fa37
Mazel <mazuel.loic@gmail.com>
2018-12-14 18:36:14 +0100

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,5 @@ module.exports = {
     watchOptions: {
       poll: true
     }
-  },
-  config.plugin('extract-css').tap(([options]) => {
-    options.filename = path.join('your/relative/asset/path', options.filename)
-    return [options]
-  })
+  }
 }
