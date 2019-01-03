ac70e2760527cddc987e53fbcd333ad576aca031
Mazel <mazuel.loic@gmail.com>
2019-01-03 18:27:23 +0100

    :book: Update Doc

--- src/App.vue
+++ src/App.vue
@@ -216,7 +216,7 @@
     },
     data () {
       return {
-        devMode: true,
+        devMode: false,
         booleanOptions: [
           'noHeader', 'autoClose', 'error', 'dark', 'overlay', 'noWeekendDays', 'noShortcuts',
           'noButton', 'onlyDate', 'range', 'onlyTime', 'inline', 'persistent', 'disabled', 'noButtonNow', 'noValueToCustomElem'
