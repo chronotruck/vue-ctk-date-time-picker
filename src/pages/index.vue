14afb5a011727ae994ba621496f1a613be19520a
William DA SILVA <william.da.silva@outlook.com>
2019-09-19 13:21:39 +0200

    feat: add nuxt demo & fix issue with window

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+      Component:
+    </h1>
+    <VueCtkDateTimePicker
+      v-model="date"
+    />
+  </div>
+</template>
+
+<script>
+  import VueCtkDateTimePicker from './../VueCtkDateTimePicker'
+
+  export default {
+    components: {
+      VueCtkDateTimePicker
+    },
+    data () {
+      return {
+        date: null
+      }
+    }
+  }
+</script>
