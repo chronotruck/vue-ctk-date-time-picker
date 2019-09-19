83207c74765878a241b13ffbb1ac9a343156dfa3
William DA SILVA <william.da.silva@outlook.com>
2019-09-19 15:03:29 +0200

    fix: avoid sending multiple "is-hidden" events (#215)

--- src/VueCtkDateTimePicker/index.vue
+++ src/VueCtkDateTimePicker/index.vue
@@ -2,7 +2,7 @@
   <div
     :id="`${$attrs.id}-wrapper`"
     ref="parent"
-    v-click-outside="() => { toggleDatePicker(false) }"
+    v-click-outside="closePicker"
     class="date-time-picker"
   >
     <!-- Input -->
@@ -31,7 +31,7 @@
     <div
       v-if="hasPickerOpen && overlay"
       class="time-picker-overlay"
-      @click.stop="toggleDatePicker(false)"
+      @click.stop="closePicker"
     />
 
     <!-- Date picker container -->
@@ -70,7 +70,7 @@
       :no-keyboard="noKeyboard"
       :right="right"
       @validate="validate"
-      @close="toggleDatePicker(false)"
+      @close="closePicker"
     />
   </div>
 </template>
@@ -156,9 +156,9 @@
         },
         set (value) {
           if (this.autoClose && this.range && (value.end && value.start)) {
-            this.toggleDatePicker(false)
+            this.closePicker()
           } else if (this.autoClose && !this.range) {
-            this.toggleDatePicker(false)
+            this.closePicker()
           }
           const newValue = this.range ? this.getRangeDateToSend(value) : this.getDateTimeToSend(value)
           this.$emit('input', newValue)
@@ -276,12 +276,27 @@
           : null
         return date ? nearestMinutes(this.minuteInterval, date, this.formatOutput).format('YYYY-MM-DD HH:mm') : null
       },
+      /**
+       * Closes the datepicker
+       * @function closePicker
+       */
+      closePicker () {
+        if (this.pickerOpen) {
+          this.$emit('is-hidden')
+          this.pickerOpen = false
+          this.setBodyOverflow(false)
+        }
+      },
       toggleDatePicker (val) {
         if (this.isDisabled) return
         const isOpen = (val === false || val === true) ? val : !this.pickerOpen
         this.setBodyOverflow(isOpen)
         this.pickerOpen = isOpen
-        this.$emit(isOpen ? 'is-shown' : 'is-hidden')
+
+        if (isOpen) {
+          this.$emit('is-shown')
+        }
+
         if (this.pickerOpen && !this.position) {
           this.pickerPosition = this.getPosition()
         }
@@ -316,7 +331,7 @@
       },
       validate () {
         this.$emit('validate')
-        this.toggleDatePicker(false)
+        this.closePicker()
       }
     }
   }
